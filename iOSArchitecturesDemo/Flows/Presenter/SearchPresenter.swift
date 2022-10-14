7cce28030c27137e13ce727c689285170d1b2e0b
Ivan Konishchev <kispaj@mail.ru>
2022-10-14 08:26:31 +0300

    Create homework lesson8

--- iOSArchitecturesDemo/Flows/Presenter/SearchPresenter.swift
+++ iOSArchitecturesDemo/Flows/Presenter/SearchPresenter.swift
@@ -28,11 +28,17 @@ protocol SearchViewOutput: AnyObject {
 //MARK: - Class presenter
 final class SearchPresenter {
     
-    private let searchService = ITunesSearchService()
+    let interactor: SearchInteractorInput
+    let router: SearchRouterInput
     weak var viewInput: ( UIViewController & SearchViewInput)?
     
+    init(interactor: SearchInteractorInput, router: SearchRouterInput) {
+        self.interactor = interactor
+        self.router = router
+    }
+    
     private func requestApps(with query: String) {
-        self.searchService.getApps(forQuery: query) { [weak self] result in
+        self.interactor.requestApps(with: query) { [weak self] result in
             guard let self = self else { return }
             self.viewInput?.throbber(show: false)
             result
@@ -48,8 +54,8 @@ final class SearchPresenter {
                     self.viewInput?.showError(error: $0)
                     
                 }
-            
         }
+        
     }
     
     
@@ -62,12 +68,14 @@ final class SearchPresenter {
 }
 
 extension SearchPresenter: SearchViewOutput {
+    
     func viewDidSearch(with query: String) {
         self.viewInput?.throbber(show: true)
         self.requestApps(with: query)
     }
+    
     func viewDidSelectApp(_ app: ITunesApp) {
-        self.openAppDetails(with: app)
+        self.router.openDetails(for: app)
     }
     
 }
---
+++ iOSArchitecturesDemo/Flows/SearchInteractorInput/SearchInteractorInput.swift
@@ -0,0 +1,22 @@
+//
+//  SearchInteractorInput.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Ivan Konishchev on 14.10.2022.
+//  Copyright © 2022 ekireev. All rights reserved.
+//
+
+import Alamofire
+
+protocol SearchInteractorInput {
+    func requestApps(with query: String, completion: @escaping (Result<[ITunesApp]>) -> Void)
+}
+
+final class SearchInteractor: SearchInteractorInput {
+    private let searchService = ITunesSearchService()
+    func requestApps(with query: String, completion: @escaping (Result<[ITunesApp]>) -> Void) {
+        self.searchService.getApps(forQuery: query, then: completion)
+        
+    }
+}
+
